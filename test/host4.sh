0a61ecc306d89ded1adc3e206fbecb68b42c9369
xhtdevil <xuhaotian@g.ucla.edu>
2017-10-31 15:25:17 -0700

    Reduce flowtable entries

--- test/host4.sh
+++ test/host4.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-ifconfig host4-eth0 10.0.4.1 netmask 255.255.255.255
-ip rule add from 10.0.4.1 table 1
-ip route add 10.0.4.1/32 dev host4-eth0 scope link table 1
-ip route add default via 10.0.4.1 dev host4-eth0 table 1
-ip route add default scope global nexthop via 10.0.4.1 dev host4-eth0
-ifconfig host4-eth1 10.0.4.2 netmask 255.255.255.255
-ip rule add from 10.0.4.2 table 2
-ip route add 10.0.4.2/32 dev host4-eth1 scope link table 2
-ip route add default via 10.0.4.2 dev host4-eth1 table 2
+ifconfig host4-eth0 10.0.4.2 netmask 255.255.255.255
+ip rule add from 10.0.4.2 table 1
+ip route add 10.0.4.2/32 dev host4-eth0 scope link table 1
+ip route add default via 10.0.4.2 dev host4-eth0 table 1
+ip route add default scope global nexthop via 10.0.4.2 dev host4-eth0
+ifconfig host4-eth1 10.0.4.1 netmask 255.255.255.255
+ip rule add from 10.0.4.1 table 2
+ip route add 10.0.4.1/32 dev host4-eth1 scope link table 2
+ip route add default via 10.0.4.1 dev host4-eth1 table 2
--- test/host7.sh
+++ test/host7.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-ifconfig host7-eth0 10.0.7.0 netmask 255.255.255.255
-ip rule add from 10.0.7.0 table 1
-ip route add 10.0.7.0/32 dev host7-eth0 scope link table 1
-ip route add default via 10.0.7.0 dev host7-eth0 table 1
-ip route add default scope global nexthop via 10.0.7.0 dev host7-eth0
-ifconfig host7-eth1 10.0.7.1 netmask 255.255.255.255
-ip rule add from 10.0.7.1 table 2
-ip route add 10.0.7.1/32 dev host7-eth1 scope link table 2
-ip route add default via 10.0.7.1 dev host7-eth1 table 2
+ifconfig host7-eth0 10.0.7.1 netmask 255.255.255.255
+ip rule add from 10.0.7.1 table 1
+ip route add 10.0.7.1/32 dev host7-eth0 scope link table 1
+ip route add default via 10.0.7.1 dev host7-eth0 table 1
+ip route add default scope global nexthop via 10.0.7.1 dev host7-eth0
+ifconfig host7-eth1 10.0.7.0 netmask 255.255.255.255
+ip rule add from 10.0.7.0 table 2
+ip route add 10.0.7.0/32 dev host7-eth1 scope link table 2
+ip route add default via 10.0.7.0 dev host7-eth1 table 2
