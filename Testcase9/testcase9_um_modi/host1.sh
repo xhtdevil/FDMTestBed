9c18c18c54be484b808ddb486dab6b0d97f05ee5
lx950627 <liuxin1995627@163.com>
2017-11-29 15:39:53 -0800

    update Testcase9

--- Testcase9/testcase9_um_modi/host1.sh
+++ Testcase9/testcase9_um_modi/host1.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-ifconfig host1-eth0 10.0.2.0 netmask 255.255.255.255
-ip rule add from 10.0.2.0 table 1
-ip route add 10.0.2.0/32 dev host1-eth0 scope link table 1
-ip route add default via 10.0.2.0 dev host1-eth0 table 1
-ip route add default scope global nexthop via 10.0.2.0 dev host1-eth0
-ifconfig host1-eth1 10.0.2.1 netmask 255.255.255.255
-ip rule add from 10.0.2.1 table 2
-ip route add 10.0.2.1/32 dev host1-eth1 scope link table 2
-ip route add default via 10.0.2.1 dev host1-eth1 table 2
+ifconfig host1-eth0 10.0.2.1 netmask 255.255.255.255
+ip rule add from 10.0.2.1 table 1
+ip route add 10.0.2.1/32 dev host1-eth0 scope link table 1
+ip route add default via 10.0.2.1 dev host1-eth0 table 1
+ip route add default scope global nexthop via 10.0.2.1 dev host1-eth0
