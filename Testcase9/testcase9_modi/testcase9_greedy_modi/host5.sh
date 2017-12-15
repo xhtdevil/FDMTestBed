2952214adae7bf1218e9dc9d6566201ff2130dc3
lx950627 <liuxin1995627@163.com>
2017-12-14 22:57:57 -0800

    update

--- Testcase9/testcase9_modi/testcase9_greedy_modi/host5.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-ifconfig host5-eth0 10.0.6.0 netmask 255.255.255.255
-ip rule add from 10.0.6.0 table 1
-ip route add 10.0.6.0/32 dev host5-eth0 scope link table 1
-ip route add default via 10.0.6.0 dev host5-eth0 table 1
-ip route add default scope global nexthop via 10.0.6.0 dev host5-eth0
